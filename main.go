97639bb6461fe9b6966afaaf02dc26c29216ba46
zhuangqianmei <zhuangqianmei@zhuangqianmeideMacBook-Pro.local>
2018-03-07 16:46:52 +0800

    lianxi

--- main.go
+++ main.go
@@ -3,6 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("HelloWorld")
+
+	fmt.Println("Hello" + "World")
+	fmt.Println("7.0/3.0=", 7.0/3.0)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
 	fmt.Println("Hi,qianmei,Nice to meet you!")
 }
